d170fc5c9f6942cefcacaf8bf4e75ccafd8f091e
Log1Xz <Micah.Teariki@gmail.com>
2015-01-25 22:16:58 +1300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -193,7 +193,7 @@
             botName: "basicBot",
             language: "english",
             chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
-            maximumAfk: 1,
+            maximumAfk: 100000,
             afkRemoval: false,
             maximumDc: 60,
             bouncerPlus: true,
